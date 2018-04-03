1c4fd270c0e042eea3ffc3687fac974eb45759e9
Paul Moore <lapilosew2003@gmail.com>
2018-04-03 22:28:42 +0100

    test moving back to a corner

--- robot_path_updated.py
+++ robot_path_updated.py
@@ -36,20 +36,6 @@ rightSensor = ult[1]
 backSensor = ult[2]
 leftSensor = ult[3]
 
-
-# String values for error messages - keep consistent
-# Shows if the wall is too near/far away/new wall found
-err_left_near = "errLeftNear"
-err_left_far_moving = "errLeftFarMoving"
-err_new_wall = "errLeftFarNewWall"
-# Shows if the invariant (vertical distance) is different (now too small/large)
-err_invariant_small = "errInvariantSmall"
-err_invariant_large = "errInvariantLarge"
-
-
-err_both = "errBoth"
-no_err = "noErr"
-
 # String values for messages passed to the path Loop
 turn_right = "Right"
 turn_left = "Left"
@@ -82,27 +68,8 @@ orientations = ["up", "right", "down", "left"]
 
 
 def moveFORWARD(speed, t):
-    front1 = getFrontDistance()
-    back1 = getBackDistance()
-    print("front1", front1)
-    print("back1", back1)
-
     frontMotor.run_timed(speed_sp = 2 * speed, time_sp = t)
     backMotor.run_timed(speed_sp = -2 * speed, time_sp = t)
-
-    time.sleep(1)
-
-    front2 = getFrontDistance()
-    back2 = getBackDistance()
-
-    print("front2", front2)
-    print("back2", back2)
-
-    # avg_value = ((front1 - front2) + (back2 - back1)) / 2
-    avg_value = (front1 - front2)
-    print("avg: ", avg_value)
-    updateLocation(avg_value)
-    print(self_location)
 
 
 def moveBACKWARD(speed, time):
@@ -207,30 +174,6 @@ def orientateBackwards():
     orientateLeft()
 
 
-def updateLocation(distance):
-    global self_location
-    if orientation == 0: # Front
-        self_location = (self_location[0],self_location[1] + distance)
-    elif orientation == 1: # Right
-        self_location = (self_location[0] + distance, self_location[1])
-    elif orientation == 2: # Back
-        self_location = (self_location[0],self_location[1] -  distance)
-    elif orientation == 3: # Left
-        self_location = (self_location[0] - distance, self_location[1])
-    #toSend(self_location[0], self_location[1], "self_location")
-
-
-def toPoint(distance):
-    if orientation == 0: # Front
-        #print(distance)
-        return (self_location[0] - distance, self_location[1])
-    elif orientation == 1: # Right
-        return (self_location[0], self_location[1] + distance)
-    elif orientation == 2: # Back
-        return (self_location[0] + distance, self_location[1])
-    elif orientation == 3: # Left
-        return (self_location[0], self_location[1] - distance)
-
 
 def getFrontDistance():
     return getSingleDistanceHelper(frontSensor)
@@ -290,146 +233,6 @@ def rotateRightSmall():
     rotateRIGHT(100, 500)
 
 
-# Check that the readings are close enough to the initial values for the left wall
-# distances are a list of [F, R, B, L]
-def checkLeftWall(left_init, distances):
-    variance = distances[3] - left_init
-    # The measurement is within +/- 1 of the initial value.
-    if -1 <= variance <= 1:
-        return no_err
-    # The measurement is 1 less than it was (implying it is too near the wall)
-    elif variance < -1:
-        return err_left_near
-    # The measurement is 1 more than it was before (implying it has gone too far away)
-    # This may be due to natural error in the instrument, or could be because of a new wall
-    elif variance > 1 and variance <= 10:
-        #TODO check threshold of 10 is appropriate here.
-        return err_left_far_moving
-    else:
-        return err_new_wall
-
-
-# Check that the readings are close enough to the initial values for the invariant (vertical distance)
-# distances are a list of [F, R, B, L]
-def checkInvariant(invariant_init, distances):
-    variance = (distances[0] + distances[2]) - invariant_init
-    # The measurements are within +/- 2 of the initial value
-    if -2 <= variance <= 2:
-        return no_err
-    # The measurements are at least 2 less than before (implying the invariant is too small now)
-    elif variance < -2:
-        return err_invariant_small
-    # The measurements are at least 2 more than before (implying the invariant is too large now)
-    else:
-        return err_invariant_large
-
-
-# Loop to follow each wall until the robot comes back to its initial location
-def pathLoop(location, init_orientation=0):
-    global self_location
-    init_location = location
-    self_location = location
-    global orientation
-    orientation = init_orientation
-    not_start = True
-    left_init = getLeftDistance()
-    invariant_init = getFrontDistance() + getBackDistance()
-    while not_start or not same_location(self_location, init_location):
-        not_start = False
-        direction, distance = wallLoop(left_init,invariant_init)
-        left_init, invariant_init = toNextWall(direction, distance)
-       # print(self_location)
-       # print(init_location)
-
-
-# Loop to follow a single wall
-# left_init is the intial distance from the wall on the left.
-# invariant_init is the intial value of distance of the front and back sensors
-# added together.
-# orientation is the current orientation relative to the left wall and the map
-
-# returns "go left/right" to show which direction the robot needs to move next.
-
-
-# TODO estimate current position based on readings vs old ones.
-# Keep track and store as (position, distance readings) tuple perhaps?
-# Main problem: orientation changes
-def wallLoop(left_init, invariant_init):
-    # Stores distance readings (all in a list, could change)
-    distance_readings = []
-    while True:
-
-        # Take in sensor readings
-        distances = getDistances()
-
-        # Check that the readings are relatively error-free (can't be 100% sure but close)
-        check_left_msg = checkLeftWall(left_init, distances)
-        check_invariant_msg = checkInvariant(invariant_init, distances)
-
-        # There was no significant error detected
-
-        if True:
-        #if check_left_msg == no_err and check_invariant_msg == no_err:
-            print("front", distances[0], "right", distances[1], "back", distances[2],  "left", distances[3])
-            # add the distances to the overall readings, (orientated correctly)
-            new_point = toPoint(distances[3])
-            distance_readings.append(new_point)
-            # Send new readings back to self-managed machine
-            #	toSend(new_point[0], new_point[1], "point")
-            # If the distance from the front is greater than the threshold, keep going
-            if distances[0] > front_threshold:
-                # Move forward by some amount (can change)
-                moveFORWARD(100, 500)
-                continue
-            # The distance is less, break from the loop and tell it to turn right next.
-            else:
-                return (turn_right, distances[0])
-        # TODO cover case of invariant error - wasn't sure how best to respond
-        # The left wall is too close (may be heading towards it)
-        if check_left_msg == err_left_near:
-            # Rotate right and move back slightly to try to fix the error
-            rotateRightSmall()
-            moveBackSmall()
-            # Loop again (will check if it's fixed)
-            continue
-
-        # The left wall is too far away - may be heading away from it as error is small
-        if check_left_msg == err_left_far_moving:
-            # Rotate left and move back slightly to try to fix the error
-            rotateLeftSmall()
-            moveBackSmall()
-            continue
-
-        # A new wall has potentially been found.
-        if check_left_msg == err_new_wall:
-            # Move forward and back slightly and check the distances are the same
-            # Done in case the error is simply due to an anomaly
-            moveForwardSmall()
-            checking_distances = getDistances()
-            moveBackSmall()
-            checking_distances2 = getDistances()
-
-            check_msg_1 = checkLeftWall(left_init, checking_distances)
-            check_msg_2 = checkLeftWall(left_init, checking_distances2)
-
-            # If both messages indicate a new wall, then it probably is
-            if check_msg_1 == err_new_wall and check_msg_2 == err_new_wall:
-                # break out of the loop
-                return (turn_left, distances[3])
-            else:
-                # Rotate left and move back slightly to try to fix the error
-                rotateLeftSmall()
-                moveBackSmall()
-                continue
-        # Some invariant error
-        #TODO fix this - shouldn't happen but might
-        else:
-            # Rotate left and move back slightly to try to fix the error
-            rotateLeftSmall()
-            moveBackSmall()
-            continue
-
-
 # This method can only be called after wallLoop()
 def same_location(location1, location2):
     if (location1[0]-location2[0])**2 + (location1[1]-location2[1])**2 < 5**2:
@@ -463,67 +266,53 @@ def moveForward(distance):
             exit()
 
 
-def find_90_rotate(power_rotation, direction):
+def find_90_rotate(power_rotation, time_rotation, direction, kind):
     """
     Test function to get the robot to rotate in place so that it finds a minimum
     Assuming robot is moving away
     """
-    print("rotating right with power: {}".format(power_rotation))
-    min_left = getLeftDistance()
+    print("rotating {} with power: {} and time: {}".format(direction, power_rotation, time_rotation))
+    min_left= getLeftDistance()
 
-    if power_rotation <= 100:
-        # Stop recursing and finish
-        return
+
+    if kind == "power":
+        if power_rotation <= 100:
+            # Stop recursing and finish
+            return
+    else:
+        if time_rotation < 100:
+            if direction == "left":
+                rotateLEFT(power_rotation, time_rotation*2)
+            else:
+                rotateRIGHT(power_rotation, time_rotation*2)
+            return
 
     while True:
         if direction == "left":
-            rotateLEFT(power_rotation, 150)
+            rotateLEFT(power_rotation, time_rotation)
         else:
-            rotateRIGHT(power_rotation, 150)
+            rotateRIGHT(power_rotation, time_rotation)
         new_left = getLeftDistance()
+        print("new left = {}".format(new_left))
         # If the value is greater the robot must have rotated past the minimum
-        if new_left <= min_left:
-            min_left = new_left
+        if new_left < min_left:
+            check_left = getLeftDistance()
+            if check_left < min_left:
+                min_left = (new_left + check_left) / 2
         else:
             break
 
     if direction == "left":
-        find_90_rotate(power_rotation / 2, "right")
-    else:
-        find_90_rotate(power_rotation / 2, "left")
-
-
-def find_90_rotate_time(time_rotation, direction):
-    """
-    Test function to get the robot to rotate in place so that it finds a minimum
-    Assuming robot is moving away
-    """
-    print("rotating right with power: {}".format(time_rotation))
-    min_left = getLeftDistance()
-
-    if time_rotation <= 100:
-        # Stop recursing and finish
-        return
-
-    while True:
-        if direction == "left":
-            rotateLEFT(150, time_rotation)
+        if kind == "power":
+            find_90_rotate(power_rotation / 2, time_rotation, "right", "power")
         else:
-            rotateRIGHT(150, time_rotation)
-        new_left = getLeftDistance()
-        # If the value is greater the robot must have rotated past the minimum
-        if new_left <= min_left:
-            min_left = new_left
-        else:
-            # Double check that the distance is indeed greater
-            check_left = getLeftDistance()
-            if check_left > min_left:
-                break
-
-    if direction == "left":
-        find_90_rotate(time_rotation / 2, "right")
+            find_90_rotate(power_rotation, time_rotation / 2, "right", "time")
     else:
-        find_90_rotate(time_rotation / 2, "left")
+        if kind == "power":
+            find_90_rotate(power_rotation / 2, time_rotation, "left", "power")
+        else:
+            find_90_rotate(power_rotation, time_rotation / 2, "left", "time")
+
 
 
 def should_rotate_left():
@@ -546,6 +335,31 @@ def should_rotate_left():
         return True
 
 
+def move_back_to_corner(time_amount, direction, approx_wall_dist):
+
+    if time_amount <= 25:
+        return
+
+    if direction == "forward":
+        while True:
+            moveFORWARD(150, time_amount)
+            left_dist = getLeftDistance()
+            if is_close(left_dist, approx_wall_dist, 10):
+                break
+        move_back_to_corner(time_amount / 2, "backward", approx_wall_dist)
+
+    else:
+        while True:
+            moveBACKWARD(150, time_amount)
+            left_dist = getLeftDistance()
+            if not is_close(left_dist, approx_wall_dist, 10):
+                break
+        move_back_to_corner(time_amount / 2, "forward", approx_wall_dist)
+
+
+
+
+
 def move_to_start_convex_corner(time_step):
     """
     Makes the robot move to the start of a convex corner
@@ -559,22 +373,7 @@ def move_to_start_convex_corner(time_step):
         else:
             find_90_rotate(time_step, "right")
 
-    # TODO test which level of recursion you should stop at.
-    if time_step < 25:
-        # Stop recursion
-        return
 
-    dist_left = getLeftDistance()
-    gone_too_far = False
-    while not gone_too_far:
-        moveBACKWARD(100, time_step)
-        new_dist_left = getLeftDistance()
-        if abs(new_dist_left - dist_left) >= left_corner_threshold:
-            gone_too_far = True
-
-    moveFORWARD(100, time_step)
-    # Recurse with smaller time steps
-    move_to_start_convex_corner(time_step/2)
 
 
 def find_new_wall(distance):
@@ -631,7 +430,7 @@ def wall_loop_2(left_init_dist, front_init_dist, wall_init_length):
 
         # If a new wall is found, return the total length of the wall
         if check_new_wall(left_init_dist, new_left):
-            wall_length = calculate_wall_length(wall_length,  )
+            wall_length = calculate_wall_length(wall_length)
             return turn_left, wall_length
 
         elif new_front <= front_threshold:
@@ -711,4 +510,6 @@ def is_very_different(num_1, num_2):
 
 
 if __name__ == "__main__":
-    find_90_rotate_time(800, "left")
+    orientateLeft()
+    init_left = getLeftDistance()
+    move_back_to_corner(400, "backward", init_left)
